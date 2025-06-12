51a4d66b7c144e5d3011cafb4b577361980a32d6
AnggerFadhilah <anggerkayafadilah@gmail.com>
2025-06-12 17:32:27 +0700

    Add files via upload

--- Void Supply/login/js/script.js
+++ Void Supply/login/js/script.js
@@ -2,27 +2,48 @@ var x = document.getElementById("login");
 var y = document.getElementById("signup");
 var z = document.getElementById("btn");
 var f = document.getElementById("forgot-password");
-var container = document.querySelector(".container"); // Get the container
+var container = document.querySelector(".container");
+
+// Ambil tombol login & signup
+var loginBtn = document.querySelectorAll(".toggle-btn")[0];
+var signupBtn = document.querySelectorAll(".toggle-btn")[1];
+
+function setActiveButton(activeBtn, inactiveBtn) {
+  activeBtn.classList.add("active");
+  activeBtn.classList.remove("inactive");
+  inactiveBtn.classList.remove("active");
+  inactiveBtn.classList.add("inactive");
+}
 
 function showLogin() {
   x.style.left = "50px";
   y.style.left = "450px";
-  f.style.left = "450px"; // Hide forgot password form
+  f.style.left = "450px";
   z.style.left = "0";
-  container.style.height = "550px"; // Reset container height for login/signup
+  container.style.height = "550px";
+
+  setActiveButton(loginBtn, signupBtn);
 }
 
 function showSignup() {
   x.style.left = "-400px";
   y.style.left = "50px";
-  f.style.left = "450px"; // Hide forgot password form
+  f.style.left = "450px";
   z.style.left = "110px";
-  container.style.height = "550px"; // Reset container height for login/signup
+  container.style.height = "550px";
+
+  setActiveButton(signupBtn, loginBtn);
 }
 
 function showForgotPassword() {
-  x.style.left = "-400px"; // Hide login form
-  y.style.left = "-400px"; // Hide signup form
-  f.style.left = "50px"; // Show forgot password form
-  container.style.height = "400px"; // Adjust container height for forgot password
+  x.style.left = "-400px";
+  y.style.left = "-400px";
+  f.style.left = "50px";
+  container.style.height = "400px";
+
+  // Nonaktifkan semua tombol toggle saat forgot password muncul
+  loginBtn.classList.remove("active");
+  loginBtn.classList.add("inactive");
+  signupBtn.classList.remove("active");
+  signupBtn.classList.add("inactive");
 }
